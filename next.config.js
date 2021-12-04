30a817729d8327d845e6745899a687569e693248
Francois roland <f.roland@applicaster.com>
2021-12-04 15:59:12 +0100

    chore: repo set up

--- next.config.js
+++ next.config.js
@@ -1,8 +1,31 @@
 // @generated: @expo/next-adapter@2.1.52
 // Learn more: https://docs.expo.io/guides/using-nextjs/
 
-const { withExpo } = require('@expo/next-adapter');
+const { withExpo } = require("@expo/next-adapter");
+const withPlugins = require("next-compose-plugins");
+const withFonts = require("next-fonts");
+const withImages = require("next-images");
 
-module.exports = withExpo({
-  projectRoot: __dirname,
-});
+const nextConfig = {
+  experimentals: { esmExternals: "loose" },
+  typescript: { ignoreBuildErrors: true }, // yeah I know
+  async headers() {
+    return [
+      {
+        source: "/",
+        // headers: [
+        //   {
+        //     key: "Content-Security-Policy",
+        //     value:
+        //       "frame-ancestors https://*.applicaster.com http://localhost:*",
+        //   },
+        // ],
+      },
+    ];
+  },
+};
+
+module.exports = withPlugins(
+  [withFonts, withImages, [withExpo, { projectRoot: __dirname }]],
+  nextConfig
+);
